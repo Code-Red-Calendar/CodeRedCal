7117504c1cbde6c031d613b70c88a772470dee1e
Si Young Mah <siyoungmah@gmail.com>
2023-08-17 16:12:23 -0700

    Add a ping route on backend

--- services/api/app/routes.py
+++ services/api/app/routes.py
@@ -1,6 +1,14 @@
 from app import app
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong'
+    })
